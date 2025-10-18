2315932eeefe3d33c4521c0eb19f0c708fafee91
XandyFaby1 <xandyfaby1@gmail.com>
2025-10-18 16:06:13 -0300

    add modulo de gerar dia da semena

--- scripts/criarItemDaLiata.js
+++ scripts/criarItemDaLiata.js
@@ -1,3 +1,4 @@
+import gerarDiaDaSemana from "./gerarDiaDaSemana.js";
 const inputItem = document.getElementById("input-item");
 let contador = 0;
 
@@ -28,20 +29,12 @@ export function criarItemDaLista() {
     containerItemDaLista.appendChild(nomeItem);
 
     itemDaLista.appendChild(containerItemDaLista);
-
-    const diaDaSemana = new Date().toLocaleDateString("pt-BR", {
-        weekday: "long"
-    });
-
-    const data = new Date().toLocaleDateString("pt-BR");
-    const hora = new Date().toLocaleTimeString('pt-BR', {
-        hour: "numeric",
-        minute: "numeric"
-    });
-    const dataCompleta = `${diaDaSemana} (${data}) às ${hora}`;
+    const dataCompleta = gerarDiaDaSemana ();
+    
     const itemData = document.createElement("p");
     itemData.innerText = dataCompleta;
     itemData.classList.add("texto-data");
+    
     itemDaLista.appendChild(itemData);
 
     return itemDaLista;
